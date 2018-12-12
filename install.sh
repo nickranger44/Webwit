89d84fea18404f3f28c2e7a406d75234a68f9d0c
nickranger44 <nicholaslueth@gmail.com>
2018-12-12 02:24:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# Assign f to be equal to the directory of "Webwit-master"
 f=$(find ~/ -type d -name "Webwit-master")
+# Change directory to the location of "Webwit-master"
 cd $f
+# Use pip to install both requests and random, just in case the user doesn't already have them.
 pip install requests
 pip install random
